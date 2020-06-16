8185c89259a7346519db6e36c763a57790d3e93d
Antoine Chalifour <achalifour@octo.com>
2020-06-16 14:25:48 +0200

    poc

---
+++ deploy-local.sh
@@ -0,0 +1,4 @@
+export MM_SERVICESETTINGS_SITEURL=http://127.0.0.1:8065
+export MM_ADMIN_USERNAME=chan
+export MM_ADMIN_PASSWORD=devdev
+make deploy
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,7 +3,18 @@ import manifest from './manifest';
 export default class Plugin {
     // eslint-disable-next-line no-unused-vars
     initialize(registry, store) {
-        // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+        const $style = document.createElement('style');
+        document.head.appendChild($style);
+
+        const styleSheet = $style.sheet;
+
+        // Insert CSS Rule
+        styleSheet.insertRule(`
+          #virtualizedPostListContent .same--root,
+           #virtualizedPostListContent .other--root{
+            display: none !important;
+          }
+        `, styleSheet.cssRules.length);
     }
 }
 
